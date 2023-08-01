3e3f72f7b15b7bceb4057e8ff207458fc20754c5
Alessandro Capotosti <alessandrocapotosti16@gmail.com>
2023-08-01 18:59:27 +0200

    -added GPS tracking and caching
    -some stuff fixed, code is a mess. would love to address this but time is short right now so i'll leave at it is (currently working). caching system is good, we tested both in VM and physical device (currently on Samsung S7 Edge which runs android 8.0); once rendered the maps and saved into the local storage, they run smoothly. Unfortunately for us Openstreetmap is worse in performances and responses trough their APIs but it is what it is and as a free to go option for developer it's very good. There's something else I'm missing for sure, code once again is taken from documentation. I'll leave the links in the report (don't forget this please)

--- app/src/main/java/com/example/lam_project/MainActivity.java
+++ app/src/main/java/com/example/lam_project/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.lam_project;
+
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -17,8 +26,34 @@ import java.io.File;
 public class MainActivity extends Activity {
     MapView map = null;
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
+    private double latitude;
+    private double longitude;
+    private boolean isCameraFixed = true;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        // Initialize osmdroid configuration (needed for caching, etc.)
+        String osmCachePath = getFilesDir().getAbsolutePath() + "/osmdroid";
+        Configuration.getInstance().load(getApplicationContext(),
+        androidx.preference.PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        Configuration.getInstance().setOsmdroidBasePath(new File(osmCachePath));
+        Configuration.getInstance().setOsmdroidTileCache(new File(osmCachePath));
+
+        // Check if the app has location permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Request the permission if not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            // If the permission is granted, start getting the location
+            startLocationUpdates();
+        }
+
 
         // Initialize osmdroid configuration (needed for caching, etc.)
 
@@ -36,16 +71,79 @@ public class MainActivity extends Activity {
         //inflate and create the map
         setContentView(R.layout.activity_main);
 
+        /*BoundingBox italyBoundingBox = new BoundingBox(35.5, 6.5, 47.1, 18.8);
+        map.zoomToBoundingBox(italyBoundingBox, false);*/
+        //map.setBuiltInZoomControls(false);
+        //IMapController mapController = map.getController();
+
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
-        BoundingBox italyBoundingBox = new BoundingBox(35.5, 6.5, 47.1, 18.8);
-        map.zoomToBoundingBox(italyBoundingBox, false);
-        map.setBuiltInZoomControls(true);
-        map.setMultiTouchControls(true);
-        IMapController mapController = map.getController();
-        mapController.setZoom(9.5);
+
+        map.setClickable(false);
+        map.setMultiTouchControls(false);
+        startLocationUpdates();
+
+        /*
         GeoPoint startPoint = new GeoPoint(44.494887, 11.3426163);
-        mapController.setCenter(startPoint);
+        mapController.setCenter(startPoint);*/
+
+    }
+
+    private void startLocationUpdates() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                // get coordinates from gps' location object (android documentation)
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                if (isCameraFixed) {
+
+                    IMapController mapController = map.getController();
+                    // Create a GeoPoint using the latitude and longitude variables
+                    GeoPoint startPoint = new GeoPoint(latitude, longitude);
+                    // Use the GeoPoint as the fixed center of the map
+                    map.getController().setCenter(startPoint);
+                    // Set the desired fixed zoom level (e.g., 12.0)
+                    mapController.setZoom(21.0);
+
+                }
+                /*
+                // Create a GeoPoint using the latitude and longitude variables
+                GeoPoint startPoint = new GeoPoint(latitude, longitude);
+                map.getController().setCenter(startPoint);*/
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // Request location updates with a minimum time interval (in milliseconds) and minimum distance (in meters)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000, // Minimum time interval between updates (e.g., 1000ms = 1 second)
+                10,   // Minimum distance between updates (e.g., 10 meters)
+                locationListener);
     }
 
     public void onResume(){

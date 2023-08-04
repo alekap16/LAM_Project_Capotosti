cea005b69a366b151908505490f53492325badd7
Alessandro Capotosti <alessandrocapotosti16@gmail.com>
2023-08-04 17:22:24 +0200

    -Added overlappings
    -was checking specifications and totally forgot overlaps were a thing. Previously I obviously tried to prevent this but when I tested before it was overlapping so little due to GPS' refreshing rates (hard to explain but GPS was refreshing at around 7-8 meters, leaving just a tiny overlap space). However I saw overlaps was not allowed at all and did not know, so I fixed this and not the squares get stored in a list (which has the 4 point coordinates) and checks if anything inside these exists or not, and accordingly, paints or not another square basing on overlaps.

--- app/src/main/java/com/example/lam_project/GridCreator.java
+++ app/src/main/java/com/example/lam_project/GridCreator.java
@@ -10,7 +10,9 @@ import org.osmdroid.views.overlay.Polygon;
 import org.osmdroid.views.overlay.Polyline;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 //is working so good cant belive its true, gotta replicate and add "meters" or smthing like that
 //so i can re-use the code for 100m squares and 1km. as for now I get one done since this is
@@ -20,46 +22,72 @@ public class GridCreator {
     // Size of the squares in meters
     private static final double SQUARE_SIZE_METERS = 10.0;
 
+    //Create each unique ID for squares, needs for overlap prevention
+    private static final List<Polygon> existingSquares = new ArrayList<>();
+
     // Method to create and display the grid overlay
     public static void createGridOverlay(MapView mapView, double latitude, double longitude) {
         if (mapView == null)
             return;
 
 
-        // Calculate the latitude and longitude differences for the square size
-        double latitudeDiff = metersToLatitude(SQUARE_SIZE_METERS);
-        double longitudeDiff = metersToLongitude(SQUARE_SIZE_METERS, latitude);
+        if (!doesSquareOverlap(latitude, longitude)) {
+            double latitudeDiff = metersToLatitude(SQUARE_SIZE_METERS);
+            double longitudeDiff = metersToLongitude(SQUARE_SIZE_METERS, latitude);
 
-        // Calculate the boundaries of the square grid
-        double latStart = latitude - 0.5 * latitudeDiff;
-        double latEnd = latitude + 0.5 * latitudeDiff;
-        double lonStart = longitude - 0.5 * longitudeDiff;
-        double lonEnd = longitude + 0.5 * longitudeDiff;
+            // Calculate the area of the square grid
+            double latStart = latitude - 0.5 * latitudeDiff;
+            double latEnd = latitude + 0.5 * latitudeDiff;
+            double lonStart = longitude - 0.5 * longitudeDiff;
+            double lonEnd = longitude + 0.5 * longitudeDiff;
 
-        // Create the square around the given coordinates
-        List<GeoPoint> squarePoints = new ArrayList<>();
-        squarePoints.add(new GeoPoint(latStart, lonStart));
-        squarePoints.add(new GeoPoint(latEnd, lonStart));
-        squarePoints.add(new GeoPoint(latEnd, lonEnd));
-        squarePoints.add(new GeoPoint(latStart, lonEnd));
+            // Create the square around the given coordinates
+            List<GeoPoint> squarePoints = new ArrayList<>();
+            squarePoints.add(new GeoPoint(latStart, lonStart));
+            squarePoints.add(new GeoPoint(latEnd, lonStart));
+            squarePoints.add(new GeoPoint(latEnd, lonEnd));
+            squarePoints.add(new GeoPoint(latStart, lonEnd));
 
-        Polygon square = new Polygon(mapView);
-        square.setPoints(squarePoints);
-        square.setStrokeColor(Color.RED);
-        square.setStrokeWidth(4f);
-        mapView.getOverlayManager().add(square);
+            Polygon square = new Polygon(mapView);
+            square.setPoints(squarePoints);
+            square.setStrokeColor(Color.RED);
+            square.setStrokeWidth(4f);
+            mapView.getOverlayManager().add(square);
 
-        mapView.invalidate();
+            existingSquares.add(square);
+            mapView.invalidate();
+        }
     }
 
     // Helper method to calculate the latitude difference for a given distance in meters
     private static double metersToLatitude(double distance) {
-        return distance / 111000.0; // 1 degree of latitude is approximately 111 kilometers or 111000 meters
+        return distance / 111000.0;
     }
 
-    // Helper method to calculate the longitude difference for a given distance in meters at a specific latitude
+    // Helper method to calculate the longitude difference for a given distance in meters at specific latitudes
     private static double metersToLongitude(double distance, double latitude) {
         double metersPerLongitudeDegree = 111320.0 * Math.cos(Math.toRadians(latitude));
         return distance / metersPerLongitudeDegree;
+    }
+
+    // Helper method that chjecks if overlaps are a thing
+    private static boolean doesSquareOverlap(double latitude, double longitude) {
+        for (Polygon square : existingSquares) {
+            List<GeoPoint> points = square.getPoints();
+            double latStart = points.get(0).getLatitude();
+            double latEnd = points.get(2).getLatitude();
+            double lonStart = points.get(0).getLongitude();
+            double lonEnd = points.get(2).getLongitude();
+
+            // Check if coordinates collides with the squares already painted for overlaps
+            for (double lat = latitude - 0.5 * metersToLatitude(SQUARE_SIZE_METERS); lat <= latitude + 0.5 * metersToLatitude(SQUARE_SIZE_METERS); lat += 0.1 * metersToLatitude(SQUARE_SIZE_METERS)) {
+                for (double lon = longitude - 0.5 * metersToLongitude(SQUARE_SIZE_METERS, latitude); lon <= longitude + 0.5 * metersToLongitude(SQUARE_SIZE_METERS, latitude); lon += 0.1 * metersToLongitude(SQUARE_SIZE_METERS, latitude)) {
+                    if (lat >= latStart && lat <= latEnd && lon >= lonStart && lon <= lonEnd) {
+                        return true; //overlap so don't print
+                    }
+                }
+            }
+        }
+        return false; // no overlap
     }
 }

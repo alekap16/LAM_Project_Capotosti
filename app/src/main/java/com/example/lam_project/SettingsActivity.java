844cac4495cef9cd6c9c2837557624b3b7a21b2c
Alessandro Capotosti <alessandrocapotosti16@gmail.com>
2023-08-17 16:13:47 +0200

    -added persistence of settings data (SharedPreferences)
    -that's it, i'm pushing because I want to mention this later for the documentation and don't want to forget about it.

--- app/src/main/java/com/example/lam_project/SettingsActivity.java
+++ app/src/main/java/com/example/lam_project/SettingsActivity.java
@@ -3,11 +3,14 @@ package com.example.lam_project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import com.example.lam_project.managers.SettingsManager;
 
 public class SettingsActivity extends Activity {
 
@@ -22,7 +25,7 @@ public class SettingsActivity extends Activity {
     private boolean isNotificationEnabled;
     private long selectedMinutes;
     private int selectedMeasurements;
-
+    private SettingsManager settingsManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +39,20 @@ public class SettingsActivity extends Activity {
         dumpDatabaseButton = findViewById(R.id.dumpDatabaseButton);
         deleteDataButton = findViewById(R.id.deleteDataButton);
 
-        notificationSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> isNotificationEnabled = isChecked);
+        settingsManager = new SettingsManager(this);
 
+        notificationSwitch.setChecked(settingsManager.isNotificationEnabled());
+        minutesSeekBar.setProgress((int) (settingsManager.getSelectedMinutes() - 5));
+        measurementsSeekBar.setProgress(settingsManager.getSelectedMeasurements() - 1);
+
+        minutesLabel.setText("Minutes for new measurements: " + settingsManager.getSelectedMinutes());
+        measurementsLabel.setText("Number of measurements: " + settingsManager.getSelectedMeasurements());
+
+        notificationSwitch.setChecked(settingsManager.isNotificationEnabled());
+
+        notificationSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            settingsManager.setNotificationEnabled(isChecked);
+        });
         minutesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -49,8 +64,12 @@ public class SettingsActivity extends Activity {
             public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                long selectedMinutes = seekBar.getProgress() + 5;
+                settingsManager.setSelectedMinutes(selectedMinutes);
+            }
         });
+
 
         measurementsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -63,7 +82,10 @@ public class SettingsActivity extends Activity {
             public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int selectedMeasurements = seekBar.getProgress() + 1;
+                settingsManager.setSelectedMeasurements(selectedMeasurements);
+            }
         });
 
         dumpDatabaseButton.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +99,11 @@ public class SettingsActivity extends Activity {
             public void onClick(View v) {
             }
         });
+    }
+
+    // I need this for later usages
+    private long convertMinutesToMilliseconds(long minutes) {
+        return minutes * 60 * 1000; // Convert minutes to milliseconds
     }
 }
 
---
+++ app/src/main/java/com/example/lam_project/managers/SettingsManager.java
@@ -0,0 +1,43 @@
+package com.example.lam_project.managers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SettingsManager {
+
+    private static final String PREF_NAME = "settings_preferences";
+    private static final String KEY_NOTIFICATION_ENABLED = "notification_enabled";
+    private static final String KEY_SELECTED_MINUTES = "selected_minutes";
+    private static final String KEY_SELECTED_MEASUREMENTS = "selected_measurements";
+
+    private SharedPreferences preferences;
+
+    public SettingsManager(Context context) {
+        preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public boolean isNotificationEnabled() {
+        return preferences.getBoolean(KEY_NOTIFICATION_ENABLED, false);
+    }
+
+    public void setNotificationEnabled(boolean isEnabled) {
+        preferences.edit().putBoolean(KEY_NOTIFICATION_ENABLED, isEnabled).apply();
+    }
+
+    public long getSelectedMinutes() {
+        return preferences.getLong(KEY_SELECTED_MINUTES, 5); // Default value 5
+    }
+
+    public void setSelectedMinutes(long minutes) {
+        preferences.edit().putLong(KEY_SELECTED_MINUTES, minutes).apply();
+    }
+
+    public int getSelectedMeasurements() {
+        return preferences.getInt(KEY_SELECTED_MEASUREMENTS, 1); // Default value 1
+    }
+
+    public void setSelectedMeasurements(int measurements) {
+        preferences.edit().putInt(KEY_SELECTED_MEASUREMENTS, measurements).apply();
+    }
+}
+
